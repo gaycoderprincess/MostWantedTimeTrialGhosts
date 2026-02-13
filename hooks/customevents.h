27a7efe19a4fd018b03d28a823cb38ce50323e79
💖 Chloe 💖 <gaycoderprincess@proton.me>
2026-02-13 17:08:47 +0100

    opponent count logic from carbon tt

--- hooks/customevents.h
+++ hooks/customevents.h
@@ -170,7 +170,11 @@ int __thiscall GetNumOpponentsHooked(GRaceParameters* pThis) {
 
 	if (bViewReplayMode) return 0;
 	if (bChallengesOneGhostOnly) return 1;
-	return nDifficulty != DIFFICULTY_EASY ? std::min(event->nNumGhosts[nDifficulty], 3) : 1; // only spawn one ghost for easy difficulty
+
+	auto count = nDifficulty != DIFFICULTY_EASY ? event->nNumGhosts[nDifficulty] : 1; // only spawn one ghost for easy difficulty
+	if (bChallengesPBGhost && event->GetPBGhost().nFinishTime != 0) count++;
+	if (count > 3) count = 3;
+	return count;
 }
 
 bool __thiscall GetIsDDayRaceHooked(GRaceParameters* pThis) {
